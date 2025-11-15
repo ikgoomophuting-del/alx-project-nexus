07fb1b04b12b4009837e908fdf46d2dd7d810b48
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-15 23:29:46 +0200

    Update and rename ecommerce_backend/products/models.py to ecommerce_backend/products/models/product.py

--- ecommerce_backend/products/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-# Day 2: We'll define Product and Category models here
-
---
+++ ecommerce_backend/products/models/product.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .category import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
