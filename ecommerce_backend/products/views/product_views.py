032d5a063d337859827c7357b6fe2c7baee39378
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-16 00:25:27 +0200

    Update product_views.py: Products an category logic

--- ecommerce_backend/products/views/product_views.py
+++ ecommerce_backend/products/views/product_views.py
@@ -1,1 +1,14 @@
+from rest_framework import viewsets, filters
+from products.models.product import Product
+from products.serializers.product_serializer import ProductSerializer
+from products.pagination.product_pagination import ProductPagination
+from products.filters.product_filters import ProductFilter
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+    filterset_class = ProductFilter
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['price', 'created_at']
 
