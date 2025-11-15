35157aa92cd3a7c4ceae68475cf00b86f67859a7
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-16 00:28:26 +0200

    Update product_filters.py: Products and category logic

--- ecommerce_backend/products/filters/product_filters.py
+++ ecommerce_backend/products/filters/product_filters.py
@@ -1,1 +1,12 @@
+import django_filters
+from products.models.product import Product
+
+class ProductFilter(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    category = django_filters.CharFilter(field_name="category__name", lookup_expr="icontains")
+
+    class Meta:
+        model = Product
+        fields = ['category']
 
