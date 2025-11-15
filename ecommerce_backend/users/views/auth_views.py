aeac2670472d2553e6669fdec63a78cae88ce520
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-16 00:34:41 +0200

    Update auth_views.py: Authentication and users

--- ecommerce_backend/users/views/auth_views.py
+++ ecommerce_backend/users/views/auth_views.py
@@ -1,1 +1,8 @@
+from rest_framework import generics
+from users.serializers.auth_serializer import RegisterSerializer
+from django.contrib.auth.models import User
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
 
