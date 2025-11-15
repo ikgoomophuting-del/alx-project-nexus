53cb2551c71119863249dff68ab96269d05d5488
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-16 00:35:31 +0200

    Update urls.py: Authentication and users

--- ecommerce_backend/users/urls.py
+++ ecommerce_backend/users/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views.auth_views import RegisterView
 
-urlpatterns = []
-
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
