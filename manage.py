139d73841aae3425cf08004b6509ae3c32d57292
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-15 17:01:52 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,17 +2,18 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_nexus_be.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Make sure it's installed and available on your PYTHONPATH."
+            "Couldn't import Django. Make sure it is installed."
         ) from exc
     execute_from_command_line(sys.argv)
 
+
 if __name__ == '__main__':
     main()
-
