ec199495227da79f86fe62f231513c701936a2a4
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 15:32:37 +0200

    Intergrating filtering, sorting and searches

--- ecommerce_backend/products/views/product_views.py
+++ ecommerce_backend/products/views/product_views.py
@@ -1,14 +1,30 @@
-from rest_framework import viewsets, filters
-from products.models.product import Product
-from products.serializers.product_serializer import ProductSerializer
-from products.pagination.product_pagination import ProductPagination
-from products.filters.product_filters import ProductFilter
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().order_by('-created_at')
+from products.models import Product
+from products.serializers.product_serializer import ProductSerializer
+from products.filters.product_filters import ProductFilter
+from products.pagination.product_pagination import ProductPagination
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
-    filterset_class = ProductFilter
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['price', 'created_at']
 
+    # Add filtering, sorting, search
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+
+    # Filters
+    filterset_class = ProductFilter
+
+    # Sorting options
+    ordering_fields = ["price", "name", "created_at"]
+    ordering = ["-created_at"]  # default newest first
+
+    # Search
+    search_fields = ["name", "description"]
