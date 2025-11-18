88d4ff27c1eb8da760141366ea975cb846e4806a
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:32:47 +0200

    Update cart serializers

--- ecommerce_backend/core/cart/serializers/cart_serializers.py
+++ ecommerce_backend/core/cart/serializers/cart_serializers.py
@@ -1,1 +1,15 @@
+from rest_framework import serializers
+from .cart_item_serializer import CartItemSerializer
+from core.cart.models.cart import Cart
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items', 'total']
+
+    def get_total(self, obj):
+        return obj.total_price()
 
