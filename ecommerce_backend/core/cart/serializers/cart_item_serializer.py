899cb9a4385211a964ad3eb5baa6d137c95cdc78
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:34:04 +0200

    Update cart item serializer

--- ecommerce_backend/core/cart/serializers/cart_item_serializer.py
+++ ecommerce_backend/core/cart/serializers/cart_item_serializer.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from core.cart.models.cart_item import CartItem
+
+class CartItemSerializer(serializers.ModelSerializer):
+    subtotal = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'subtotal']
+
+    def get_subtotal(self, obj):
+        return obj.subtotal()
 
