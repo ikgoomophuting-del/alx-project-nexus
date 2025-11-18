aeaa63976809727612ae64bab12e4fb2628e3a50
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 15:28:03 +0200

    Product Filters

--- ecommerce_backend/products/filters/product_filters.py
+++ ecommerce_backend/products/filters/product_filters.py
@@ -1,12 +1,13 @@
-import django_filters
-from products.models.product import Product
+from django_filters import rest_framework as filters
+from products.models import Product
 
-class ProductFilter(django_filters.FilterSet):
-    min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
-    max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
-    category = django_filters.CharFilter(field_name="category__name", lookup_expr="icontains")
+
+class ProductFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
+    category = filters.CharFilter(field_name="category__name", lookup_expr="icontains")
+    in_stock = filters.BooleanFilter(field_name="is_available")
 
     class Meta:
         model = Product
-        fields = ['category']
-
+        fields = ["min_price", "max_price", "category", "in_stock"]
