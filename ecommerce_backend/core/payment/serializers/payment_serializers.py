227fd7140f84170d02159dbc13db8add14a6a6c3
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 17:17:19 +0200

    Update Payment serializers

--- ecommerce_backend/core/payment/serializers/payment_serializers.py
+++ ecommerce_backend/core/payment/serializers/payment_serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from core.payment.models.payment import Payment
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ['id', 'user', 'amount', 'reference', 'status', 'created_at']
 
