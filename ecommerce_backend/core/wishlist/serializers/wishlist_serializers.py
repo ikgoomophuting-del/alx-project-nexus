e164921be6ea5fb3515d87f0aeb31c86655554f5
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:38:40 +0200

    Update wishlist serializers

--- ecommerce_backend/core/wishlist/serializers/wishlist_serializers.py
+++ ecommerce_backend/core/wishlist/serializers/wishlist_serializers.py
@@ -1,1 +1,11 @@
+from rest_framework import serializers
+from core.wishlist.models.wishlist import Wishlist
+from products.serializers.product_serializer import ProductSerializer
+
+class WishlistSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'user', 'products']
 
