2d8babf6d24d0a0e800eca340d14becfb37fc66a
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:37:55 +0200

    Update wishlist model

--- ecommerce_backend/core/wishlist/models/wishlist.py
+++ ecommerce_backend/core/wishlist/models/wishlist.py
@@ -1,1 +1,15 @@
+from django.db import models
+from django.conf import settings
+from products.models.product import Product
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="wishlist"
+    )
+    products = models.ManyToManyField(Product, related_name='wishlisted_by')
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
 
