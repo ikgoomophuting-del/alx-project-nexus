358d7ec2ccca66f9d205c18d43b088d16c76a41f
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 14:50:00 +0200

    Register, Login serializers

--- ecommerce_backend/users/serializers/auth_serializers.py
+++ ecommerce_backend/users/serializers/auth_serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from users.models import User
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["email", "username", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        return User.objects.create_user(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            password=validated_data["password"],
+        )
 
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(email=data["email"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+        return user
