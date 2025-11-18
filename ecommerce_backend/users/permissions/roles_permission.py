7ad01b5bbb0641f58299ddb7f55ab7fa5c6bd1cb
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 15:50:32 +0200

    Permission Classes

--- ecommerce_backend/users/permissions/roles_permission.py
+++ ecommerce_backend/users/permissions/roles_permission.py
@@ -1,1 +1,32 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdmin(BasePermission):
+    """
+    Allows access only to admin/staff users.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+
+class IsOwner(BasePermission):
+    """
+    Allows access only to the owner of a given object.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Admins get full access.
+    Everyone else gets read-only (GET).
+    """
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
 
