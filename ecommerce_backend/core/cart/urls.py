2b1f39495bb257d1db73365ea61a80bd8e01e9ad
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:36:35 +0200

    Update cart routes

--- ecommerce_backend/core/cart/urls.py
+++ ecommerce_backend/core/cart/urls.py
@@ -1,1 +1,21 @@
+from django.urls import path
+from core.cart.views.cart_views import CartViewSet
+
+cart = CartViewSet.as_view({
+    'get': 'retrieve'
+})
+
+add_item = CartViewSet.as_view({
+    'post': 'add_item'
+})
+
+remove_item = CartViewSet.as_view({
+    'post': 'remove_item'
+})
+
+urlpatterns = [
+    path('', cart, name='cart'),
+    path('add/', add_item, name='add-item'),
+    path('remove/', remove_item, name='remove-item'),
+]
 
