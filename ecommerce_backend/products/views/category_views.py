25a722f04c250445441bd139052962839ffb547f
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:06:20 +0200

    Apply permission to catergory views

--- ecommerce_backend/products/views/category_views.py
+++ ecommerce_backend/products/views/category_views.py
@@ -1,8 +1,21 @@
 from rest_framework import viewsets
 from products.models.category import Category
 from products.serializers.category_serializer import CategorySerializer
+from users.permissions.roles_permissions import IsAdminOrReadOnly
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    
 
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
