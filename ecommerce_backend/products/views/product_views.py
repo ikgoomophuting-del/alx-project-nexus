ca7efb8b5c85a5c189a57521a69100727ceeb7b1
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:04:08 +0200

    Apply permission to product views

--- ecommerce_backend/products/views/product_views.py
+++ ecommerce_backend/products/views/product_views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from users.permissions.roles_permissions import IsAdminOrReadOnly
 
 from products.models import Product
 from products.serializers.product_serializer import ProductSerializer
@@ -29,5 +30,35 @@ class ProductListView(generics.ListAPIView):
     # Search
     search_fields = ["name", "description"]
 
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+
+    filterset_class = ProductFilter
+    ordering_fields = ["price", "name", "created_at"]
+    ordering = ["-created_at"]
+    search_fields = ["name", "description"]
+
+    # Only admins can POST new products
+    permission_classes = [IsAdminOrReadOnly]
+
+
+    class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # Only Admins can update/delete
+    permission_classes = [IsAdminOrReadOnly]
+
+
 # Only admins can POST new products
     permission_classes = [IsAdminOrReadOnly]
+
