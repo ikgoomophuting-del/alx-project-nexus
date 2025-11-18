5647f90bb7ac674ef67402d245cf93a8f0fe69c1
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-18 16:39:25 +0200

    Update wishlist views

--- ecommerce_backend/core/wishlist/views/wishlist_views.py
+++ ecommerce_backend/core/wishlist/views/wishlist_views.py
@@ -1,1 +1,28 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from core.wishlist.models.wishlist import Wishlist
+from core.wishlist.serializers.wishlist_serializer import WishlistSerializer
+
+class WishlistViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request):
+        wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
+        return Response(WishlistSerializer(wishlist).data)
+
+    def add(self, request):
+        wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
+        product_id = request.data.get("product_id")
+
+        wishlist.products.add(product_id)
+        return Response({"message": "Added to wishlist"})
+
+    def remove(self, request):
+        wishlist = request.user.wishlist
+        product_id = request.data.get("product_id")
+
+        wishlist.products.remove(product_id)
+        return Response({"message": "Removed from wishlist"})
 
