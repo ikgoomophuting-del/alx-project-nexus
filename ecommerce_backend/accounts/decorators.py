82ab4c16b75a2d5ce40cae1a968f3e57130153c1
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-22 14:13:00 +0200

    Implement role_required decorator for access control
    Add a role_required decorator to restrict access based on user roles.

--- ecommerce_backend/accounts/decorators.py
+++ ecommerce_backend/accounts/decorators.py
@@ -1,1 +1,13 @@
+# accounts/decorators.py
+
+from django.http import HttpResponseForbidden
+
+def role_required(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            if request.user.role not in allowed_roles:
+                return HttpResponseForbidden("You are not allowed to access this resource.")
+            return view_func(request, *args, **kwargs)
+        return wrapper
+    return decorator
 
