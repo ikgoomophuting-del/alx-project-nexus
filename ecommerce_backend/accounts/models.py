c3ecca6ce79004284f190eaab6cfd709bffa4d0b
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-25 16:52:55 +0200

    Enhance User model with email and verification fields

--- ecommerce_backend/accounts/models.py
+++ ecommerce_backend/accounts/models.py
@@ -1,15 +1,19 @@
-# accounts/models.py
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+
     ROLE_CHOICES = (
-        ('admin', 'Admin'),
-        ('parent', 'Parent'),
-        ('teacher', 'Teacher'),
-        ('student', 'Student'),
+        ("admin","Admin"),
+        ("seller","Seller"),
+        ("customer","Customer"),
     )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="customer")
 
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
+    def __str__(self):
+        return self.email
