b35ee9116c2157a86ea0134cc32ec0fdf51d8bb0
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-25 16:53:29 +0200

    Add custom UserAdmin for User model

--- ecommerce_backend/accounts/admin.py
+++ ecommerce_backend/accounts/admin.py
@@ -1,1 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("email","username","role","is_staff")
+    search_fields = ("email","username")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email","password")}),
+        ("Personal", {"fields": ("username",)}),
+        ("Permissions", {"fields": ("is_active","is_staff","is_superuser","role")}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",),"fields": ("email","username","password1","password2","role")}),
+    )
 
