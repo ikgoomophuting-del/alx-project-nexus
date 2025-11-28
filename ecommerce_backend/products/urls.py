a32c95bd16a4026ecf79d0f36d75b8ca133f1121
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-29 00:15:36 +0200

    Refactor imports for view sets in urls.py

--- ecommerce_backend/products/urls.py
+++ ecommerce_backend/products/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, ReviewViewSet
+from products.views.product_views import ProductViewSet
+from products.views.category_views import CategoryViewSet
+from products.views.review_views import ReviewViewSet  # if you have this
 
 router = DefaultRouter()
+
 router.register(r"products", ProductViewSet, basename="products")
 router.register(r"categories", CategoryViewSet, basename="categories")
 router.register(r"reviews", ReviewViewSet, basename="reviews")
+
 urlpatterns = router.urls
