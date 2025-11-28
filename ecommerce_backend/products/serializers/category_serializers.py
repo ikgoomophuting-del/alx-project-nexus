babdc3df800fbb965f3000f95be7716cbf706478
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-28 23:53:29 +0200

    Add Product and Review serializers with fields

--- ecommerce_backend/products/serializers/category_serializers.py
+++ ecommerce_backend/products/serializers/category_serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
-from products.models.category import Category
+from .models import Product, Category, Review
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ["id","name","slug","description"]
 
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.email")
+    class Meta:
+        model = Review
+        fields = ["id","product","user","rating","comment","created_at"]
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.email')
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Product
+        fields = ["id","name","slug","description","price","stock","is_available","category","category_id","owner","reviews","created_at","updated_at"]
+    def create(self, validated_data):
+        category_id = validated_data.pop("category_id", None)
+        if category_id:
+            validated_data["category"] = Category.objects.get(pk=category_id)
+        validated_data["owner"] = self.context["request"].user
+        return super().create(validated_data)
+    def update(self, instance, validated_data):
+        category_id = validated_data.pop("category_id", None)
+        if category_id is not None:
+            instance.category = Category.objects.get(pk=category_id)
+        return super().update(instance, validated_data)
