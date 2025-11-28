fe281b5fe73f037c18dffcd724726e73d0214add
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-28 23:48:58 +0200

    Update category.py

--- ecommerce_backend/products/models/category.py
+++ ecommerce_backend/products/models/category.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
-    
-    def __str__(self):
-        return self.name
-
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=160, unique=True, blank=True)
+    description = models.TextField(blank=True)
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args,**kwargs)
+    def __str__(self): return self.name
