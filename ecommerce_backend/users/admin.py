186c3ce33a5137c1b25163f0b2ec9af909550c82
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-28 23:43:29 +0200

    Register Profile model in Django admin

--- ecommerce_backend/users/admin.py
+++ ecommerce_backend/users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import User
+from .models import Profile
 
 admin.site.register(User)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user","phone")
+
+
+
+
