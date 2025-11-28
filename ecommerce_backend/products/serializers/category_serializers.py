1ce22dfc52f5430d2a03ae655fe9fa3b33c28805
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-28 23:54:52 +0200

    Remove ProductSerializer from category_serializers.py

--- ecommerce_backend/products/serializers/category_serializers.py
+++ ecommerce_backend/products/serializers/category_serializers.py
@@ -12,22 +12,3 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ["id","product","user","rating","comment","created_at"]
 
-class ProductSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.email')
-    category = CategorySerializer(read_only=True)
-    category_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
-    reviews = ReviewSerializer(many=True, read_only=True)
-    class Meta:
-        model = Product
-        fields = ["id","name","slug","description","price","stock","is_available","category","category_id","owner","reviews","created_at","updated_at"]
-    def create(self, validated_data):
-        category_id = validated_data.pop("category_id", None)
-        if category_id:
-            validated_data["category"] = Category.objects.get(pk=category_id)
-        validated_data["owner"] = self.context["request"].user
-        return super().create(validated_data)
-    def update(self, instance, validated_data):
-        category_id = validated_data.pop("category_id", None)
-        if category_id is not None:
-            instance.category = Category.objects.get(pk=category_id)
-        return super().update(instance, validated_data)
