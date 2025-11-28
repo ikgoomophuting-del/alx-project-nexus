4adc21b8cc0257654b5fb460b6bd2518c1724077
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-29 00:59:39 +0200

    Update review_serializers.py

--- ecommerce_backend/products/serializers/review_serializers.py
+++ ecommerce_backend/products/serializers/review_serializers.py
@@ -1,1 +1,13 @@
+# products/serializers/review_serializer.py
+from rest_framework import serializers
+from products.models.review import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ["id", "product", "user", "rating", "comment", "created_at"]
+        read_only_fields = ["id", "user", "created_at"]
 
