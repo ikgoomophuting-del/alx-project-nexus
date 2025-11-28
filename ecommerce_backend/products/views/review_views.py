039c2e0e4253017c691dfa73b5d418ea15472de8
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-29 01:00:08 +0200

    Update review_views.py

--- ecommerce_backend/products/views/review_views.py
+++ ecommerce_backend/products/views/review_views.py
@@ -1,1 +1,20 @@
+# products/views/review_views.py
+from rest_framework import generics, permissions
+from products.models.review import Review
+from products.serializers.review_serializer import ReviewSerializer
+
+
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objects.select_related("product", "user").all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.select_related("product", "user").all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
