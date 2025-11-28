01062073b0dd76cf39f279923a539ee295ecb458
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-29 01:03:11 +0200

    Updated permission to category views

--- ecommerce_backend/products/views/category_views.py
+++ ecommerce_backend/products/views/category_views.py
@@ -1,16 +1,12 @@
-from rest_framework import viewsets
+# products/views/category_views.py
+from rest_framework import generics
 from products.models.category import Category
 from products.serializers.category_serializer import CategorySerializer
 from users.permissions.roles_permissions import IsAdminOrReadOnly
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    
-
 class CategoryListView(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().order_by("name")
     serializer_class = CategorySerializer
     permission_classes = [IsAdminOrReadOnly]
 
