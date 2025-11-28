4ccadd54f251c7a265dec0461477c8b12608631d
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-29 00:30:39 +0200

    Change Django settings module for manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
-
-
 def main():
+    
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_nexus_be.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Make sure it is installed."
-        ) from exc
+        raise
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
