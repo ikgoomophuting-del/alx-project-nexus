5e2e7fb2c9f403a693666e528ebb6a393707409c
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-29 18:33:33 +0200

    Update wsgi.py

--- ecommerce_backend/ecommerce/wsgi.py
+++ ecommerce_backend/ecommerce/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce_backend.ecommerce.settings")
 
 application = get_wsgi_application()
-
