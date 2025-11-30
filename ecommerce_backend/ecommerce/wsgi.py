7a3abd3cfcec79827fecbcfe36a970d4d606847e
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-30 14:28:39 +0200

    Update wsgi.py

--- ecommerce_backend/ecommerce/wsgi.py
+++ ecommerce_backend/ecommerce/wsgi.py
@@ -1,6 +1,12 @@
 import os
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce_backend.ecommerce.settings")
+# Add project root to Python path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
 
 application = get_wsgi_application()
