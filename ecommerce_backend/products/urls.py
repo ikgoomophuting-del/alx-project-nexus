29f27a8759d5e9b49432913ccc9b093da293babf
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-12-03 15:34:57 +0200

    Update urls.py

--- ecommerce_backend/products/urls.py
+++ ecommerce_backend/products/urls.py
@@ -1,12 +1,13 @@
-from rest_framework.routers import DefaultRouter
-from products.views.product_views import ProductViewSet
-from products.views.category_views import CategoryViewSet
-from products.views.review_views import ReviewViewSet  # if you have this
+from django.urls import path
+from products.views.product_views import ProductListCreateView, ProductDetailView
+from products.views.category_views import CategoryListCreateView, CategoryDetailView
+from products.views.review_views import ReviewListCreateView, ReviewDetailView
 
-router = DefaultRouter()
-
-router.register(r"products", ProductViewSet, basename="products")
-router.register(r"categories", CategoryViewSet, basename="categories")
-router.register(r"reviews", ReviewViewSet, basename="reviews")
-
-urlpatterns = router.urls
+urlpatterns = [
+    path("products/", ProductListCreateView.as_view(), name="product-list"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("categories/", CategoryListCreateView.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryDetailView.as_view(), name="category-detail"),
+    path("reviews/", ReviewListCreateView.as_view(), name="review-list"),
+    path("reviews/<int:pk>/", ReviewDetailView.as_view(), name="review-detail"),
+]
